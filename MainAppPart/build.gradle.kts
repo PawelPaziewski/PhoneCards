3e83f82a80a415ecbc9c60fa8c58f1395e9c1960
PawelPaziewski <lrpapawel@gmail.com>
2021-12-27 14:24:04 +0100

    Add mongo db database support

--- MainAppPart/build.gradle.kts
+++ MainAppPart/build.gradle.kts
@@ -16,6 +16,8 @@ repositories {
 dependencies {
     implementation("org.axonframework:axon-spring-boot-starter:4.5.5")
     implementation("org.springframework.boot:spring-boot-starter-web:2.6.2")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb:2.6.2")
+    runtimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
 
     testImplementation("org.axonframework:axon-test:4.5.5")
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.2")
