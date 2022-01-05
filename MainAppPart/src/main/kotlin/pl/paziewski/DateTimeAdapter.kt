3bf4677c2d72e4e89542e042f53ebd14f37fd527
PawelPaziewski <lrpapawel@gmail.com>
2022-01-05 14:00:01 +0100

    Implement custom DateTimeAdapter converters

--- MainAppPart/src/main/kotlin/pl/paziewski/DateTimeAdapter.kt
+++ MainAppPart/src/main/kotlin/pl/paziewski/DateTimeAdapter.kt
@@ -1,6 +1,12 @@
 package pl.paziewski
 
+import org.springframework.core.convert.converter.Converter
+import org.springframework.data.convert.ReadingConverter
+import org.springframework.data.convert.WritingConverter
 import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.*
+
 
 //The adapter will be developed according to needed functionality
 class DateTimeAdapter(val date: LocalDateTime) {
@@ -24,5 +30,19 @@ class DateTimeAdapter(val date: LocalDateTime) {
 
     override fun hashCode(): Int {
         return date.hashCode()
+    }
+}
+
+@WritingConverter
+class DataTimeAdapterToLocalDateTimeConverter : Converter<DateTimeAdapter, LocalDateTime> {
+    override fun convert(source: DateTimeAdapter): LocalDateTime {
+        return source.date
+    }
+}
+
+@ReadingConverter
+class DateToDateTimeAdapterConverter : Converter<Date, DateTimeAdapter> {
+    override fun convert(source: Date): DateTimeAdapter {
+        return DateTimeAdapter(LocalDateTime.ofInstant(source.toInstant(), ZoneId.systemDefault()))
     }
 }
--- MainAppPart/src/main/kotlin/pl/paziewski/PhoneCardsApplication.kt
+++ MainAppPart/src/main/kotlin/pl/paziewski/PhoneCardsApplication.kt
@@ -2,7 +2,10 @@ package pl.paziewski
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @SpringBootApplication
@@ -12,4 +15,16 @@ class PhoneCardsApplication
 fun main(args: Array<String>) {
     val ctx = runApplication<PhoneCardsApplication>(*args)
     ctx.getBean(MongoTemplate::class.java).db.drop()
+}
+
+
+@Configuration
+class Config {
+
+    @Bean
+    fun mongoConversions(): MongoCustomConversions {
+        return MongoCustomConversions(listOf(DataTimeAdapterToLocalDateTimeConverter(),
+            DateToDateTimeAdapterConverter()))
+    }
+
 }
